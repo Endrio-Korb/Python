921f4adb14f2ec0928c74d5de45364e15f31991b
Endrio-Korb <alexkorb95@gmail.com>
2023-04-12 18:05:30 -0300

    Python

--- modulo02-python-com-banco-de-dados/Aula4/script_02.py
+++ modulo02-python-com-banco-de-dados/Aula4/script_02.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 # Dicionário com os produtos
 produtos = [
@@ -30,20 +31,21 @@ def criar_tabela_produto(cursor):
     )
     """
     cursor.execute(comando)
-    print("Tabela tb_produtos criada com sucesso.")
+    #print("Tabela tb_produtos criada com sucesso.")
 
 
 # Função que cria a tabela de pedidos
 def criar_tabela_pedidos(cursor):
     comando = """
     CREATE TABLE IF NOT EXISTS tb_pedidos(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER NOT NULL,
         data_hora TIMESTAMP NOT NULL,
-        observacoes TEXT 
+        observacoes TEXT,
+        PRIMARY KEY (id, data_hora)
     );
     """
     cursor.execute(comando)
-    print("Tabela tb_pedidos criada com sucesso.")
+    #print("Tabela tb_pedidos criada com sucesso.")
 
 
 
@@ -60,7 +62,7 @@ def criar_tabela_pedido_itens(cursor):
         );
     """
     cursor.execute(comando)
-    print("Tabela tb_pedidos_itens criada com sucesso.")
+    #print("Tabela tb_pedidos_itens criada com sucesso.")
 
 
 # Função que mostra os dados da tabela tb_pedidos
@@ -68,9 +70,11 @@ def mostrar_tb_produtos(cursor):
     comando = """
     SELECT * FROM tb_produtos;
     """
-    resultado = cursor.execute(comando)
-    produtos = resultado.fetchall()
-    return produtos
+    cursor.execute(comando)
+    resultado = cursor.fetchall()
+    saida = resultado
+    for item in resultado:
+        print(item)
 
 
 # Função que insire dados em tb_produtos
@@ -81,7 +85,7 @@ def inserir_tb_produtos(cursor):
         preco = produto.get("preco")
         comando = f"INSERT INTO tb_produtos(nome, preco) VALUES ('{nome}', '{preco}')"
         cursor.execute(comando)    
-        print(f"O produto {nome} foi inserido com sucesso.")
+        #print(f"O produto {nome} foi inserido com sucesso.")
 
 if __name__ == "__main__":
     
@@ -89,9 +93,9 @@ if __name__ == "__main__":
     cursor = conexao.cursor()
     cursor.execute("PRAGMA foreign_key = ON;")
 
-    #criar_tabela_produto(cursor)
-    #criar_tabela_pedidos(cursor)
-    #criar_tabela_pedido_itens(cursor)
+    criar_tabela_produto(cursor)
+    criar_tabela_pedidos(cursor)
+    criar_tabela_pedido_itens(cursor)
 
     #inserir_tb_produtos(cursor)
     #conexao.commit()
@@ -111,10 +115,23 @@ if __name__ == "__main__":
         try: 
             opcao = int(input("Opção: "))
             if opcao == 1:
-                print(mostrar_tb_produtos(cursor))
                 # Para cadastrar um novo pedido, é interessante mostrar a lista de produtos disponíveis
                 # Também colocar uma validação que verifica se o ID do produto existe
+                mostrar_tb_produtos(cursor)
+                
+                comida = int(input("Qual comida deseja pedir:"))
 
+                comando = f"""
+                SELECT * From tb_produtos WHERE id = {comida};
+                """
+                cursor.execute(comando)
+
+                data = datetime.date.today()
+                comando = f"""
+                INSERT INTO tb_pedidos (id, data_hora) VALUES ({comida}, {data});
+                """
+                cursor.execute(comando)
+                conexao.commit()
                 # Quando um pedido for finalizado, devemos salvar os dados em 2 tabelas:
                 # tb_pedidos, tb_pedidos_itens
 
