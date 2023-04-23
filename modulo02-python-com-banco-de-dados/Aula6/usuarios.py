0a6fc937975ca839b90ec8067e8f7ef6e923e206
Endrio-Korb <alexkorb95@gmail.com>
2023-04-23 16:26:40 -0300

    Python

--- modulo02-python-com-banco-de-dados/Aula6/usuarios.py
+++ modulo02-python-com-banco-de-dados/Aula6/usuarios.py
@@ -22,6 +22,35 @@ def criar_perfil_usuario(usuario, nome, sexo):
     session.add(perfil_usuario)
     session.commit
 
+
+def lista_usuarios():
+
+    # Consultador todos os registros (SELECT * FROM)
+    resultado = session.query(Usuario).all()
+    if len(resultado):
+        print("Não há usuário cadastrados no sistema.")
+
+
 if __name__ == "__main__":
 
-    criar_usuario("ana@email.com", "123456", "Ana", "F")
+    while True:
+
+        print("Informe a opção desejada:")
+        print("1 - Lista todos os usuários")
+        print("2 - Cadastrar um Usuário")
+        print("3 - Excluir um Usuário")
+        print("4 - Atulizar dados do Usuário")
+        print("5 - Sair")
+
+        opcao = int(input("OPÇÃO: "))
+
+        if opcao == 1:
+            lista_usuarios()
+
+        elif opcao == 2:
+            email = input("Informe o email do usuário:")
+            senha = input("Informe a senha do usuário")
+            nome = input("Informe o nome do usuário")
+            sexo = input("Infore o sexo do usuário")
+
+            criar_usuario(email, senha, nome, sexo)
