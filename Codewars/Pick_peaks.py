5065907c4995403a273d2fff7cdb1a14229acd84
Endrio-Korb <alexkorb95@gmail.com>
2023-04-27 19:41:00 -0300

    Python

--- Codewars/Pick_peaks.py
+++ Codewars/Pick_peaks.py
@@ -7,41 +7,56 @@ def pick_peaks(arr):
     posicoes = []
     anterior = 0
     posterior = 1
+    contador = 0
+    anterior_ind = 0
+
+    x = (arr)
+
+    for i in x:
+        try:
+            arr.index(i)
+            d_posicao.update({posicao:i})
+            lista_posicao.append(posicao)
+            posicao += 1
+
+
+        except ValueError:
+            pass
+
 
     for i in arr:
+
         if anterior <= i:
             try:
                 anterior_ind = (arr.index(i) - 1 )
+
                 if anterior_ind == -1:
                     anterior_ind = 0
+                    contador += 1
+
+                if contador >= 2:
+                    arr = arr[i:]
+
+                anterior_ind = (arr.index(i) - 1)
                 anterior = arr[anterior_ind]
-                posterior_ind = arr.index(anterior)
-                posterior_ind += 2
+                posterior_ind = anterior_ind + 2
                 posterior = arr[posterior_ind]
             except ValueError:
                 pass
 
             if anterior < i and posterior < i:
                 picos.append(i)
-                posicoes.append(arr.index(i))
+                for chave, valor in d_posicao.items():
+                    if valor == i:
+                        posicoes.append(chave)
+        anterior = 1
+
+
+    return picos, posicoes
 
 
 
 
-
-
-    # x = (arr)
-
-    # for i in x:
-    #     try:
-    #         arr.index(i)
-    #         d_posicao.update({posicao:i})
-    #         lista_posicao.append(posicao)
-    #         posicao += 1
-
-
-    #     except ValueError:
-    #         pass
 
 
     
