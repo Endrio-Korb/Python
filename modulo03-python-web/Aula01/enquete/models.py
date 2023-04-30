1d148d9e141f2d48f48e1296354389a390a5e8a9
Endrio-Korb <alexkorb95@gmail.com>
2023-04-30 18:39:53 -0300

    Python

--- modulo03-python-web/Aula01/enquete/models.py
+++ modulo03-python-web/Aula01/enquete/models.py
@@ -6,3 +6,7 @@ class Pergunta(models.Model):
 
     texto = models.CharField(max_length=200)
     data_de_publicacao = models.DateField("Data de Publicação")
+
+    # A classe Meta representa as configurações da model
+    class Meta:
+        db_table = "tb_perguntas"
