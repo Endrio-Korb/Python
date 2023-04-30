7b63726253063ceaf23ffa70f90822beaf5874a0
Endrio-Korb <alexkorb95@gmail.com>
2023-04-30 18:32:49 -0300

    Python

--- modulo03-python-web/Aula01/enquete/apps.py
+++ modulo03-python-web/Aula01/enquete/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EnquetesConfig(AppConfig):
+class EnqueteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'enquetes'
+    name = 'enquete'
