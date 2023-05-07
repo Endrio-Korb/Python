521a7138dded9b3debf0d92cf45cbb96bfd10f85
Endrio-Korb <alexkorb95@gmail.com>
2023-05-07 18:32:20 -0300

    Python

--- modulo03-desenvolvimento-web-com-python/20230507-aula02/enquetes/views.py
+++ modulo03-desenvolvimento-web-com-python/20230507-aula02/enquetes/views.py
@@ -1,7 +1,7 @@
 from random import randint
 
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 
 
@@ -19,14 +19,22 @@ def numero_da_sorte(request):
 def index(request):
     ultimas_cinco_perguntas = Pergunta.objects.order_by("-data_de_publicacao")[:5]
 
-    template = loader.get_template("enquetes/index.html")
+    # template = loader.get_template("enquetes/index.html")
+    # contexto = {"ultimas_cinco_perguntas": ultimas_cinco_perguntas}
+
+    # return HttpResponse(template.render(contexto, request))
     contexto = {"ultimas_cinco_perguntas": ultimas_cinco_perguntas}
 
-    return HttpResponse(template.render(contexto, request))
+    return render(request, "enquetes/index.html",context = contexto)
 
 
 def detalhes(request, pergunta_id):
-    return HttpResponse(f"Você está acessando os detalhes da pergunta {pergunta_id}")
+    # pk é primary key
+    # A função get_objetc_or_404 faz a consulta pelo registro com a pk informada.
+    # Se não encontrar, gera um erro 404 NOT FOUND
+    pergunta = get_object_or_404(Pergunta,pk=pergunta_id)
+
+    return render(request, "enquetes/detalhes.html", {"pergunta":pergunta})
 
 
 def resultados(request, pergunta_id):
