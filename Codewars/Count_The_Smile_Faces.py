152076726b388c78ec9c6df51fe7d91b09134e62
Endrio-Korb <alexkorb95@gmail.com>
2023-05-08 21:34:28 -0300

    Python

--- Codewars/Count_The_Smile_Faces.py
+++ Codewars/Count_The_Smile_Faces.py
@@ -1,5 +1,6 @@
 def count_smileys(arr):
-    smile = [':D', ':~)', ';-D', ':)', ';~D']
+    smile = [':)',':-)',':~)', ';)', ';-)', ';~)',
+             ':D', ':-D', ':~D', ';D', ';-D', ';~D']
     s = 0
     
     for i in smile:
@@ -7,17 +8,6 @@ def count_smileys(arr):
             if i == j:
                 s += 1
     return s
-
-def count_smileys2(arr):
-    not_smile = [';(', ':>', ':}', ':[', ';*', ':$', ':O', ':;', ':(']
-    saida = []
-    for i in arr:
-        saida.append(i)
-    for i in arr:
-        for j in not_smile:
-            if i == j:
-                saida.remove(j)
-    return len(saida)
 
 
 # https://www.codewars.com/kata/583203e6eb35d7980400002a/train/python
