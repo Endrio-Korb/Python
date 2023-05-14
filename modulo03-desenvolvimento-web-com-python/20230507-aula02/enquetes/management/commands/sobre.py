985a5bcba2219ec3c38bc3968d70c4de6a78dbb4
Endrio-Korb <alexkorb95@gmail.com>
2023-05-14 17:47:38 -0300

    Python

---
+++ modulo03-desenvolvimento-web-com-python/20230507-aula02/enquetes/management/commands/sobre.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    
+    # Texto que aparece na ajuda do comando
+    help = "Informações sobre o projeto"
+
+    # O método handle é responsável por executar o códigio do comando chamado
+    def handle(self, *args, **options):
+        saida = """
+        Sistema de enquetes
+        Proway 2023
+        """
+
+        self.stdout.write(saida)
