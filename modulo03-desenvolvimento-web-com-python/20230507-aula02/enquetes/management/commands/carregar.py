c115968e8cf506d446a2040a7f78b0de51c224a9
Endrio-Korb <alexkorb95@gmail.com>
2023-05-14 18:41:52 -0300

    Python

---
+++ modulo03-desenvolvimento-web-com-python/20230507-aula02/enquetes/management/commands/carregar.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+from django.core.management.base import BaseCommand
+
+from enquetes.models import Pergunta, Opcao
+
+
+class Command(BaseCommand):
+
+    help = "Carregar arquivos e salva no banco de dados"
+
+    def add_arguments(self, parser):
+        parser.add_argument("arquivo", nargs=1, type=str)
+
+    def handle(self, *args, **options):
+        
+        arquivo = options.get("arquivo")
+        caminho_arquivo = os.path.join(os.getcwd(), arquivo)
+
+        with open(caminho_arquivo, "r", encoding="utf-8") as _f:
+
+            conteudo_arquivo = json.load(_f)
+
+            for pergunta, opcoes in conteudo_arquivo.items():
+                print(pergunta, opcoes)
