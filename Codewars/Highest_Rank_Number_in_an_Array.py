320f4075001e3aee2e9f7c3221c89e2fa2b67a0c
Endrio-Korb <alexkorb95@gmail.com>
2023-05-19 18:47:27 -0300

    Python

--- Codewars/Highest_Rank_Number_in_an_Array.py
+++ Codewars/Highest_Rank_Number_in_an_Array.py
@@ -1,37 +1,29 @@
 def highest_rank(arr):
     dicio = {}
+    maior1 = 0
+    saida = []
     
     for numero in arr:
         if dicio.get(numero):
             dicio[numero] += 1
         else:
             dicio[numero] = 1
-    
-    num1 = 0
-    num2 = 0
-    vezes = 0
-    for chave, valor in dicio.items():
-        if valor >= vezes:
-            vezes = valor
-            num1 = chave
-    
-    maior2 = 0
-    
-    for i in dicio:
-        if i > maior2:
-            maior2 = i
-            num2 = i
 
-    if num2 >= num1 and maior2 < vezes:
-        return num2
-    elif num1 <= num2 and vezes < maior2:
-        return num1
-    
+    for valor in dicio.values():
+        if valor > maior1:
+            maior1 = valor
+        
+    for chave, valor in dicio.items():
+        if valor == maior1:
+            saida.append(chave)
+
+    return max(saida)
 
 
 # https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004/train/python
 
+if __name__ == "__main__":
 
-arr = [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]
+    arr = [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]
 
-print(highest_rank(arr))
+    print(highest_rank(arr))
